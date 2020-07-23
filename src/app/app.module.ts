c7eb87fe435859f1f72ab46c347b35bd37df44e2
josepereza <josepereza66@gmail.com>
2020-07-23 15:41:02 +0200

    first2 commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EditarComponent } from './components/editar/editar.component';
+import {  ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    HeaderComponent,
+    AddTodoComponent,
+    AboutComponent,
+    EditarComponent,
+    
+  ],
+  imports: [
+    BrowserModule, AppRoutingModule,
+     HttpClientModule, FormsModule, NgbModule,
+    ReactiveFormsModule,NgbModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
